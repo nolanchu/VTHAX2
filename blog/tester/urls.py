8ee9b3d76f14cb166668c1cf0da3001cf7763df1
nolanchu <nolan.chu.2012@gmail.com>
2023-09-16 13:21:08 -0400

    workinger

--- blog/tester/urls.py
+++ blog/tester/urls.py
@@ -8,6 +8,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('display/', views.DisplayRecordsView.as_view()),
     path('step1/', views.step1, name='step1'),
     path('step2/', views.step2, name='step2'),
     path('step3/', views.step3, name='step3'),
--- blog/tester/views.py
+++ blog/tester/views.py
@@ -1,19 +1,18 @@
-# from django.shortcuts import render, redirect
-# from .forms import HealthSurveyForm
-# from django.views.generic import TemplateView
-# import json
-# from .models import HealthSurvey
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+import json
 
-# def calculate_percentile(queryset, field_name, value):
-#     filters = {f"{field_name}__lt": value}
-#     count_below = queryset.filter(**filters).count()
-#     total = queryset.count()
-
-
-#     if total == 0:
-#         return 0
-
-#     return (count_below / total) * 100
+def calculate_percentile(queryset, field_name, latest_survey):
+    if not str(latest_survey.data[field_name]).isdecimal():
+        return -1
+    total = queryset.count()
+    count_below = 0
+    for e in queryset:
+        if e.data[field_name] < latest_survey.data[field_name]:
+            count_below += 1
+    if total == 0:
+        return 1
+    return (count_below / total) * 100
 
 # def health_survey(request):
 #     if request.method == 'POST':
@@ -26,37 +25,46 @@
 
 #     return render(request, 'tester/health_survey_template.html', {'form': form})
 
-# class DisplayRecordsView(TemplateView):
-#     template_name = 'display_records_template.html'
+class DisplayRecordsView(TemplateView):
+    template_name = 'display_records_template.html'
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         # Get latest survey
-#         latest_survey = HealthSurvey.objects.latest('id')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get latest survey
+        latest_survey = MyModel.objects.latest('id')
 
-#         # Calculate percentiles for latest survey
-#         cigarettes_per_day_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "cigarettes_per_day", latest_survey.cigarettes_per_day)
-#         exercise_per_week_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "exercise_per_week", latest_survey.exercise_per_week)
-#         reaction_time_ms_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "reaction_time_ms", latest_survey.reaction_time_ms)
-#         alcohol_per_week_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "alcohol_per_week", latest_survey.alcohol_per_week)
-#         sitting_duration_per_day_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "sitting_duration_per_day", latest_survey.sitting_duration_per_day)
+        percentiles = {}
+        for attribute in latest_survey.data:
+            percentiles[attribute] = calculate_percentile(MyModel.objects.exclude(id=latest_survey.id), attribute, latest_survey)
+        # print(percentiles)
 
-#         obj = latest_survey
-#         context['data'] = json.dumps(
-#             {
-#                 'cigarettes': obj.cigarettes_per_day,
-#                 'cigarettes_percentile': cigarettes_per_day_percentile,
-#                 'exercise': obj.exercise_per_week.total_seconds(),
-#                 'exercise_percentile': exercise_per_week_percentile,
-#                 'reaction': obj.reaction_time_ms,
-#                 'reaction_percentile': reaction_time_ms_percentile,
-#                 'alcohol': obj.alcohol_per_week,
-#                 'alcohol_percentile': alcohol_per_week_percentile,
-#                 'sitting': obj.sitting_duration_per_day.total_seconds(),
-#                 'sitting_percentile': sitting_duration_per_day_percentile,
-#             }
-#     )
-#         return context
+        context['data'] = json.dumps(percentiles)
+        return context
+
+        # for key in 
+        # Calculate percentiles for latest survey
+        # cigarettes_per_day_percentile = calculate_percentile(MyModel.objects.exclude(id=latest_survey.id), "cigarettes_per_day", latest_survey.cigarettes_per_day)
+        # exercise_per_week_percentile = calculate_percentile(MyModel.objects.exclude(id=latest_survey.id), "exercise_per_week", latest_survey.exercise_per_week)
+        # reaction_time_ms_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "reaction_time_ms", latest_survey.reaction_time_ms)
+        # alcohol_per_week_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "alcohol_per_week", latest_survey.alcohol_per_week)
+        # sitting_duration_per_day_percentile = calculate_percentile(HealthSurvey.objects.exclude(id=latest_survey.id), "sitting_duration_per_day", latest_survey.sitting_duration_per_day)
+
+    #     obj = latest_survey
+    #     context['data'] = json.dumps(
+    #         {
+    #             'cigarettes': obj.cigarettes_per_day,
+    #             'cigarettes_percentile': cigarettes_per_day_percentile,
+    #             'exercise': obj.exercise_per_week.total_seconds(),
+    #             'exercise_percentile': exercise_per_week_percentile,
+    #             'reaction': obj.reaction_time_ms,
+    #             'reaction_percentile': reaction_time_ms_percentile,
+    #             'alcohol': obj.alcohol_per_week,
+    #             'alcohol_percentile': alcohol_per_week_percentile,
+    #             'sitting': obj.sitting_duration_per_day.total_seconds(),
+    #             'sitting_percentile': sitting_duration_per_day_percentile,
+    #         }
+    # )
+    #     return context
 
 # def display_records(request):
 #     # Get latest survey
