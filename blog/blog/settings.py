bc9c3e7b665d57f87cff47b00fa310b1f2379801
nolanchu <nolan.chu.2012@gmail.com>
2023-09-16 23:39:47 -0400

    static files

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -139,8 +139,8 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "tester/static/tester")]
+STATIC_URL = '/tester/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "tester/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
