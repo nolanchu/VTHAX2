b6051a5457c351824ef005db1a6056c5e05bf5ff
nolanchu <nolan.chu.2012@gmail.com>
2023-09-17 00:23:48 -0400

    pleasee!!

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -139,7 +139,9 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/tester/'
+# just /tester/ made it go to /tester/tester/x.png instead of tester/static/tester/x.png
+# /static/tester/ makes it go to /static/tester/tester/burger
+STATIC_URL = '/tester/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "tester/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
