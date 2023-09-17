64fcb94e03c96a78468e818e7697f672b1a23f1a
nolanchu <nolan.chu.2012@gmail.com>
2023-09-16 20:23:27 -0400

    smoking

--- blog/tester/urls.py
+++ blog/tester/urls.py
@@ -6,11 +6,12 @@ from django.urls import path
 # ]
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('display/', views.DisplayRecordsView.as_view()),
     path('step/6', views.finished),
     path('step/<str:step_num>', views.step),
     path('finished/', views.finished, name='finished'),  # You'll need to create this view
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
