ce6b9ff2747349c6072c9886b80252945eb2ae4e
nolanchu <nolan.chu.2012@gmail.com>
2023-09-17 01:15:35 -0400

    errant slash???

--- blog/tester/urls.py
+++ blog/tester/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('step/<str:step_num>', views.step),
     path('finished/', views.finished, name='finished'),  # You'll need to create this view
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
 
